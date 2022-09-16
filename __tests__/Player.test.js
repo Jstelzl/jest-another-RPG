aee7433da103bdd866f9287ec8e3eec31267c700
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-09-16 12:23:55 -0400

    fixed failing test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,17 @@
+const Postion = require('../lib/Postion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Postion());
+
 const Player = require('../lib/Player');
 
 test('create a player object', () => {
     const player = new Player('Dave');
+
+    expect(Player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 
 
     expect(player.name).toBe('Dave');
